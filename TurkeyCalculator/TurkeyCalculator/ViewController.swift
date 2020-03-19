c4ad74896b420e0c46c8d323ea27e0818a61a374
Your Name Here <youremailhere>
2020-03-18 23:32:44 -0400

    Tureky calculator

--- TurkeyCalculator/TurkeyCalculator/ViewController.swift
+++ TurkeyCalculator/TurkeyCalculator/ViewController.swift
@@ -20,11 +20,56 @@ class ViewController: UIViewController {
     
     @IBAction func calculateButtonPressed(_ sender: Any) {
         
+        // get the input from the user in turkeyweighTextField
+        guard let turkeyWeightString = turkeyWeightTextField.text else {return}
+        
+        guard let turkeyWeight = Double(turkeyWeightString) else {
+            print("Invalid Number")
+            return
+        }
+        
+        // Calculate cook time based on turkey weigh and metric system used
+        var durationInMinutes: Double
+        
+        if metricButton.isSelected {
+            durationInMinutes = cookTimeInKilograms(turkeyWeight)
+        } else {
+            durationInMinutes = cookTimeInPounds(turkeyWeight)
+        }
+        
+        // Present the cook time to the user in the cooktimeTextField
+        let durationInHours = durationInMinutes / 60
+          cookTimeTextField.text = "\(durationInHours) minutes"
+//        cookTimeTextField.text = "\(durationInMinutes) minutes"
     }
     
     @IBAction func metricButtonPressed(_ sender: Any) {
         
+        // Conditional Downcast
+        
+        guard let button = sender as? UIButton else {
+            return
+        }
+        button.isSelected.toggle()
+        
     }
     
     // Helper functions
+    
+    func cookTimeInPounds(_ weight: Double) -> Double {
+        let durationInMinutes = weight * 15.0
+        return durationInMinutes
+    }
+    func cookTimeInKilograms(_ weight: Double) -> Double {
+        var durationINMinutes: Double
+        
+        if weight >= 4 {
+            durationINMinutes = 20 * weight + 90
+        } else {
+            durationINMinutes = 20 * weight + 70
+        }
+        
+        return durationINMinutes
+    }
+    
 }
